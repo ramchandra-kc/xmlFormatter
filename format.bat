e4e0752b18209f23cba870589add2fc207d5b260
ram <ramchandra@arthasoftsolutions.com>
2023-03-16 17:58:54 +0545

    updated to accept multiple folders

--- format.bat
+++ format.bat
@@ -5,6 +5,22 @@ set "folder1=%~1"
 
 if "%folder1%" neq "" (
     for /r "%folder1%" %%f in (*.xml *.rels) do (
-        xmllint --format "%%f" --output "%%f"
+        "./xmllint.exe" --format "%%f" --output "%%f"
+    )
+)
+
+set "folder2=%~2"
+
+if "%folder2%" neq "" (
+    for /r "%folder2%" %%f in (*.xml *.rels) do (
+        "./xmllint.exe" --format "%%f" --output "%%f"
+    )
+)
+
+set "folder3=%~3"
+
+if "%folder3%" neq "" (
+    for /r "%folder3%" %%f in (*.xml *.rels) do (
+        "./xmllint.exe" --format "%%f" --output "%%f"
     )
 )
